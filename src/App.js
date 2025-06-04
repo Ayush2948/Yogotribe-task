7f82049d1384f694b10b204158d091cfc6e6afd2
AYUSH PATIL <ayush.patil2021@vitstudent.ac.in>
2025-06-04 20:32:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [fact, setFact] = useState('');
+
+  const fetchCatFact = async () => {
+    try {
+      const response = await fetch(process.env.REACT_APP_URL);
+      const data = await response.json();
+      setFact(data.fact);
+    } catch (error) {
+      console.error('Error fetching cat fact:', error);
+      setFact('Hire Me!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      
+      <p style={{ marginTop: '20px' , backgroundColor: 'lightgray', padding: '10px', fontSize:'35.55px' }}>{fact}</p>
+      <button onClick={fetchCatFact} style={{marginBottom: '20px', fontSize: '40px'}}>
+        Get Cat Fact
+      </button>
+
     </div>
+    
   );
 }
 
